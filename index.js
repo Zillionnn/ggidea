e9058a26b9df1229ab834329bb61cfaa2046d4ea
zillionnn <skyestzhang@163.com>
2020-12-02 00:05:45 +0800

    u

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function generateIdea() {
-    document.getElementById("idea_text").innerHTML = '123';
+    let result = '';
+    console.log(gametypes);
+    for (let key in gametypes) {
+        let randomIdx = Math.floor(Math.random() * gametypes[key].length);
+        result += gametypes[key][randomIdx] + ', '
+    }
+    document.getElementById("idea_text").innerHTML = result;
 }
